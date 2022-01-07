9594c64f405dde04fb482cdda913526696430d02
acalebwilson24 <caleb@natureslaboratory.co.uk>
2022-01-07 16:43:03 +0000

    added max width to blog post

--- wp-content/themes/beevital/single.php
+++ wp-content/themes/beevital/single.php
@@ -1,6 +1,6 @@
 <?php get_header(); the_post(); ?>
 <div class="container__outer">
-    <div class="container__inner mw_1146" id="content-page">
+    <div class="container__inner mw_1146" id="content-page" style="max-width: 800px">
         <h1 class="heading large"><?php the_title(); ?></h1>
 <!--         <ul id="categories-list"><?php bv__outputPostCategoryList(get_the_ID()); ?></ul> -->
         <?php the_content(); ?>

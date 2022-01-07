286ed69afb6b74a43405890217f363fc4eb24ae5
Jack Barber <jack@natureslaboratory.co.uk>
2022-01-07 14:41:58 +0000

    blog tweaks

--- wp-content/themes/beevital/page.php
+++ wp-content/themes/beevital/page.php
@@ -1,6 +1,6 @@
 <?php get_header(); the_post(); ?>
 <div class="container__outer">
-    <div class="container__inner mw_1146" id="content-page">
+    <div class="container__inner mw_1146 <?php if (is_blog()) { echo 'mw_800'; } ?>" id="content-page">
         <h1 class="heading large"><?php the_title(); ?></h1>
         <?php the_content(); ?>
     </div>

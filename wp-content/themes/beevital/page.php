5431b615d9afb111a1b4587f1ccbba8ebbbbc921
acalebwilson24 <caleb@natureslaboratory.co.uk>
2022-01-10 11:59:49 +0000

    added basic faq styling

--- wp-content/themes/beevital/page.php
+++ wp-content/themes/beevital/page.php
@@ -1,6 +1,7 @@
 <?php get_header(); the_post(); ?>
+<?php global $post; ?>
 <div class="container__outer">
-    <div class="container__inner mw_1146" id="content-page">
+    <div class="container__inner mw_1146" id="content-page" <?= $post->post_name == "faq" ? "style='max-width: 800px'" : null ?>>
         <h1 class="heading large"><?php the_title(); ?></h1>
         <?php the_content(); ?>
     </div>

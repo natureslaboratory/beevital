c8b0fca0d6b9160611114aa21c737e97ca05944b
Jack Barber <jack@natureslaboratory.co.uk>
2022-01-12 12:24:26 +0000

    Update header.php

--- wp-content/themes/beevital/header.php
+++ wp-content/themes/beevital/header.php
@@ -5,7 +5,13 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title><?php wp_title(); ?></title>
     <?php wp_head(); ?>
-
+	<script type="text/javascript">
+	    (function(c,l,a,r,i,t,y){
+	        c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+	        t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+	        y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+	    })(window, document, "clarity", "script", "a55dnlc90a");
+	</script>
 	<style>
 		.thwmsc-buttons {
 

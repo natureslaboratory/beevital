6f0d34a612539580fb55b246675a712a3346eff5
Jack Barber <jack@natureslaboratory.co.uk>
2022-01-12 12:57:40 +0000

    Update index.php

--- wp-content/themes/beevital/index.php
+++ wp-content/themes/beevital/index.php
@@ -6,7 +6,7 @@
 <div class="container__outer">
 <?php if ($posts = bv__getLatestBlogPosts(20, false)) : $p = 0 ?>
     <div class="container__inner mw_1146">
-		<h1>Posts Tagged '<?php echo $tag; ?>'</h1>
+		<h1 class="heading large">Posts Tagged '<?php echo $tag; ?>'</h1>
         <?php foreach ($posts as $post) :  ?>
             <div class="remedy_tab_section">
 

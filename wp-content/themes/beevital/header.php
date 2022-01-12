6082bce70aec03d6cf28d77ff23538a996a7cb5e
Jack Barber <jack@natureslaboratory.co.uk>
2022-01-12 16:43:15 +0000

    Update header.php

--- wp-content/themes/beevital/header.php
+++ wp-content/themes/beevital/header.php
@@ -7,7 +7,23 @@
     <?php wp_head(); ?>
     <!-- FOR PINTEREST -->
     <meta name="p:domain_verify" content="f64c6cc71bda1715105675e41de13d50"/>
-    
+    <!-- Pinterest Tag -->
+	<script>
+	!function(e){if(!window.pintrk){window.pintrk = function () {
+	window.pintrk.queue.push(Array.prototype.slice.call(arguments))};var
+	  n=window.pintrk;n.queue=[],n.version="3.0";var
+	  t=document.createElement("script");t.async=!0,t.src=e;var
+	  r=document.getElementsByTagName("script")[0];
+	  r.parentNode.insertBefore(t,r)}}("https://s.pinimg.com/ct/core.js");
+	pintrk('load', '2613183829422', {em: '<user_email_address>'});
+	pintrk('page');
+	</script>
+	<noscript>
+	<img height="1" width="1" style="display:none;" alt=""
+	  src="https://ct.pinterest.com/v3/?event=init&tid=2613183829422&pd[em]=<hashed_email_address>&noscript=1" />
+	</noscript>
+	<!-- end Pinterest Tag -->
+
 	<script type="text/javascript">
 	    (function(c,l,a,r,i,t,y){
 	        c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};

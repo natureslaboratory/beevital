a650ad267e3070cda186dc6dbf6aea6767f4567c
Jack Barber <jack@natureslaboratory.co.uk>
2022-01-19 21:10:50 +0000

    favicon

--- wp-content/themes/beevital/header.php
+++ wp-content/themes/beevital/header.php
@@ -88,6 +88,14 @@
 	    font-size:1em;
     }
     </style>
+
+	<link rel="apple-touch-icon" sizes="180x180" href="/images/favicon/apple-touch-icon.png">
+	<link rel="icon" type="image/png" sizes="32x32" href="/images/favicon/favicon-32x32.png">
+	<link rel="icon" type="image/png" sizes="16x16" href="/images/favicon/favicon-16x16.png">
+	<link rel="manifest" href="/images/favicon/site.webmanifest">
+	<link rel="mask-icon" href="/images/favicon/safari-pinned-tab.svg" color="#5bbad5">
+	<meta name="msapplication-TileColor" content="#da532c">
+	<meta name="theme-color" content="#ffffff">
     
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/@fancyapps/ui@4.0/dist/fancybox.css" />
 </head>

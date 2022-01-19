f4b0a3c68b522f0324b4faaf397fd4eebead512b
Jack Barber <jack@natureslaboratory.co.uk>
2022-01-19 22:21:56 +0000

    Update functions.php

--- wp-content/themes/beevital/functions.php
+++ wp-content/themes/beevital/functions.php
@@ -143,7 +143,7 @@ add_filter( 'rwmb_meta_boxes', function ( $meta_boxes ) {
                 'id'   => $prefix . 'howtotake',
             ],
             [
-                'type' => 'wysiwyg',
+                'type' => 'textarea',
                 'name' => esc_html__( 'Herbal Docs', 'herbaldocs' ),
                 'id'   => $prefix . 'herbaldocs',
             ]

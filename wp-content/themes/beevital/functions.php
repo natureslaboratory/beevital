c812a172e2acb3038fb738e09437caa43bc0b28f
Jack Barber <jack@natureslaboratory.co.uk>
2022-01-20 09:42:38 +0000

    Update functions.php

--- wp-content/themes/beevital/functions.php
+++ wp-content/themes/beevital/functions.php
@@ -1313,3 +1313,11 @@ function bv_wc_pagination($args) {
 add_filter("woocommerce_pagination_args", "bv_wc_pagination");
 
 add_filter( 'wp_lazy_loading_enabled', '__return_false' );
+
+function defer_parsing_of_js( $url ) {
+    if ( is_user_logged_in() ) return $url; //don't break WP Admin
+    if ( FALSE === strpos( $url, '.js' ) ) return $url;
+    if ( strpos( $url, 'jquery.js' ) ) return $url;
+    return str_replace( ' src', ' defer src', $url );
+}
+add_filter( 'script_loader_tag', 'defer_parsing_of_js', 10 );

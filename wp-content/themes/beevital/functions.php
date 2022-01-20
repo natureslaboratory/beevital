b5ee1a77c1b4dc054f4b5e5ab0582b732b05d22c
Jack Barber <jack@natureslaboratory.co.uk>
2022-01-20 09:39:21 +0000

    Update functions.php

--- wp-content/themes/beevital/functions.php
+++ wp-content/themes/beevital/functions.php
@@ -1313,11 +1313,3 @@ function bv_wc_pagination($args) {
 add_filter("woocommerce_pagination_args", "bv_wc_pagination");
 
 add_filter( 'wp_lazy_loading_enabled', '__return_false' );
-
-//Remove Gutenberg Block Library CSS from loading on the frontend
-function smartwp_remove_wp_block_library_css(){
-    wp_dequeue_style( 'wp-block-library' );
-    wp_dequeue_style( 'wp-block-library-theme' );
-    wp_dequeue_style( 'wc-block-style' ); // Remove WooCommerce block CSS
-} 
-add_action( 'wp_enqueue_scripts', 'smartwp_remove_wp_block_library_css', 100 );

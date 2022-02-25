a7d7218e074d28d577944ab75291c646afff0abd
Jack Barber <jack@natureslaboratory.co.uk>
2022-02-25 16:38:10 +0000

    Update header.php

--- wp-content/themes/beevital/header.php
+++ wp-content/themes/beevital/header.php
@@ -245,6 +245,10 @@
 		font-size:0.9rem;
 		margin-bottom:1rem;
 	}
+	
+	#product_main .description .block_cta{
+		margin-top:1rem;
+	}
     </style>
 
 	<link rel="apple-touch-icon" sizes="180x180" href="/wp-content/themes/beevital/images/favicon/apple-touch-icon.png">

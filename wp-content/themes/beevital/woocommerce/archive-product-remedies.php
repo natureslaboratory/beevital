7389c902c8f48ce9ef85282dce70f9c73a76fef7
Jack Barber <jack@natureslaboratory.co.uk>
2022-08-17 12:21:30 +0100

    Update archive-product-remedies.php

--- wp-content/themes/beevital/woocommerce/archive-product-remedies.php
+++ wp-content/themes/beevital/woocommerce/archive-product-remedies.php
@@ -30,27 +30,27 @@
 
             <div class="vertical_icons">
 
-                <div class="icon">
-                    <i class="far fa-seedling"></i>
-                </div>
-
-                <div class="icon">
-                    <i class="fas fa-plus"></i>
-                </div>
-
-                <div class="icon">
-                    <img src="<?php bv__getThemeImageUrl('global/misc/vertical_icons/bee.png'); ?>" alt="Bee" />
-                </div>
-
-                <div class="icon">
-                    <i class="fas fa-equals"></i>
-                </div>
-
-                <div class="icon">
-                    <i class="far fa-heart"></i>
-                </div>
-
-            </div>
+	            <div class="icon">
+	                <svg aria-hidden="true" focusable="false" data-prefix="fas" data-icon="leaf" class="svg-inline--fa fa-leaf fa-w-18" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 576 512"><path fill="currentColor" d="M546.2 9.7c-5.6-12.5-21.6-13-28.3-1.2C486.9 62.4 431.4 96 368 96h-80C182 96 96 182 96 288c0 7 .8 13.7 1.5 20.5C161.3 262.8 253.4 224 384 224c8.8 0 16 7.2 16 16s-7.2 16-16 16C132.6 256 26 410.1 2.4 468c-6.6 16.3 1.2 34.9 17.5 41.6 16.4 6.8 35-1.1 41.8-17.3 1.5-3.6 20.9-47.9 71.9-90.6 32.4 43.9 94 85.8 174.9 77.2C465.5 467.5 576 326.7 576 154.3c0-50.2-10.8-102.2-29.8-144.6z"></path></svg>
+	            </div>
+	
+	            <div class="icon">
+	                <svg aria-hidden="true" focusable="false" data-prefix="fas" data-icon="plus" class="svg-inline--fa fa-plus fa-w-14" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512"><path fill="currentColor" d="M416 208H272V64c0-17.67-14.33-32-32-32h-32c-17.67 0-32 14.33-32 32v144H32c-17.67 0-32 14.33-32 32v32c0 17.67 14.33 32 32 32h144v144c0 17.67 14.33 32 32 32h32c17.67 0 32-14.33 32-32V304h144c17.67 0 32-14.33 32-32v-32c0-17.67-14.33-32-32-32z"></path></svg>
+	            </div>
+	
+	            <div class="icon">
+	                <img src="<?php bv__getThemeImageUrl('global/misc/vertical_icons/bee.png'); ?>" alt="Bee Icon" />
+	            </div>
+	
+	            <div class="icon">
+	                <svg aria-hidden="true" focusable="false" data-prefix="fas" data-icon="equals" class="svg-inline--fa fa-equals fa-w-14" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512"><path fill="currentColor" d="M416 304H32c-17.67 0-32 14.33-32 32v32c0 17.67 14.33 32 32 32h384c17.67 0 32-14.33 32-32v-32c0-17.67-14.33-32-32-32zm0-192H32c-17.67 0-32 14.33-32 32v32c0 17.67 14.33 32 32 32h384c17.67 0 32-14.33 32-32v-32c0-17.67-14.33-32-32-32z"></path></svg>
+	            </div>
+	
+	            <div class="icon">
+	                <svg aria-hidden="true" focusable="false" data-prefix="fas" data-icon="heart" class="svg-inline--fa fa-heart fa-w-16" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><path fill="currentColor" d="M462.3 62.6C407.5 15.9 326 24.3 275.7 76.2L256 96.5l-19.7-20.3C186.1 24.3 104.5 15.9 49.7 62.6c-62.8 53.6-66.1 149.8-9.9 207.9l193.5 199.8c12.5 12.9 32.8 12.9 45.3 0l193.5-199.8c56.3-58.1 53-154.3-9.8-207.9z"></path></svg>
+	            </div>
+	
+	        </div>
 
         </div>
         <!-- END OF PAGE INTRO -->
@@ -210,12 +210,16 @@
                 </div>
 
                 <div class="column image_wrapper">
+	                
+	                <?php if(bv__isRemedy('cold-and-flu')){ ?>
 
                     <div class="image" style="background-image: url('<?php bv__getThemeImageUrl('remedies/cold_and_flu.jpg'); ?>');"></div>
 
                     <div class="caption">
-                        How Does Propolis Support the Immune System?
+                        Propolis Can Support the Immune System Against Colds and Flu
                     </div>
+                    
+					<?php } ?>
 
                 </div>
 

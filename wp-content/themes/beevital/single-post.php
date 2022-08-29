bea56fa36eac72c87a9657f92bde97caeadf08f6
Jack Barber <jack@natureslaboratory.co.uk>
2022-08-29 13:20:48 +0100

    Update single-post.php

--- wp-content/themes/beevital/single-post.php
+++ wp-content/themes/beevital/single-post.php
@@ -7,7 +7,7 @@
 		    <?php 
 			    if ( function_exists('get_field') && get_field('producthighlight') ):
 			        $product = get_field('producthighlight');
-			        $product = wc_get_product($product['ID');
+			        $product = wc_get_product($product['ID'));
 			        print_r($product);
 			    endif;
 		    ?>

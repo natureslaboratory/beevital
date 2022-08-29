75b9d35a0cf6ffd36ac19750b0769bec3945fef8
Jack Barber <jack@natureslaboratory.co.uk>
2022-08-29 14:27:09 +0100

    highlight

--- wp-content/themes/beevital/single-post.php
+++ wp-content/themes/beevital/single-post.php
@@ -9,16 +9,17 @@
 			        $productObj = get_field('producthighlight');
 			        $product = wc_get_product($productObj);
 			        $image = wp_get_attachment_image_src( get_post_thumbnail_id( $productObj ), 'single-post-thumbnail' );
-/*
+
 			        echo '
 			        <div class="product-highlight">
-			        <h2><a href="/product/'.$product->get_slug().'">'.$product->get_name().'</a></h2>
-			        <p>&pound;'; 
-			        echo $product->get_price()*1.2;
-			        echo '</p>
+			        <p>Product Highlight</p>
 			        <img src="'.$image[0].'" alt="'.$product->get_name().'" />
+			        <h2><a href="/product/'.$product->get_slug().'">'.$product->get_name().'</a></h2>
+			        <p class="price">&pound;'; 
+			        echo number_formace($product->get_price()*1.2,2);
+			        echo '</p>
 			        </div>';
-*/
+
 			    endif;
 		    ?>
 		    <div>

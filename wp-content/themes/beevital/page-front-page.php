c83cdf3300ff68531b47f8903fc6410f513d136a
Jack Barber <jack@natureslaboratory.co.uk>
2022-09-02 11:38:56 +0100

    Update page-front-page.php

--- wp-content/themes/beevital/page-front-page.php
+++ wp-content/themes/beevital/page-front-page.php
@@ -476,7 +476,7 @@
 
                         <div class="column image">
 
-                            <img src="<?php echo the_post_thumbnail($post->ID, 'large'); ?>" loading="lazy" alt="" />
+                            <?php echo get_the_post_thumbnail($post->ID, 'large'); ?>
 
                         </div>
 
